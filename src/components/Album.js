8bbba8ce3f787a1482992fd4859d2a66d0ad910c
jordan.crutcher <jordan.crutcher@gmail.com>
2018-07-01 15:15:11 -0500

    assignment jams album component complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -32,8 +32,17 @@ class Album extends Component {
             <col id="song-duartion-cloumn" />
           </colgroup>
           <tbody>
+          {
+            this.state.album.songs.map( (song, index) =>
+              <tr>
+                <td>{index+1}</td>
+                <td>{song.title}</td>
+                <td>{song.duration}</td>
+              </tr>  
+            )
+          }
           </tbody>
-        </table>  
+        </table>
       </section>
     );
   }
