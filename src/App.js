ba0aef8e7a786f0d0430c1ef4cd17235df7c64cd
jordan.crutcher <jordan.crutcher@gmail.com>
2018-07-14 10:31:39 -0500

    styling started

--- src/App.js
+++ src/App.js
@@ -11,10 +11,9 @@ class App extends Component {
       <div className="App">
         <header>
           <nav>
-            <Link to='/'>Landing</Link>
+            <Link to='/'><img src="./assets/images/bloc_jams_logo.png" alt="blocjamslogo" /></Link>
             <Link to='/library'>Library</Link>
           </nav>
-          <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
